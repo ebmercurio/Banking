ce2bee02a1a3f7ddc25710ce774c43ca6102dd07
Evan Mercurio <EMercurio@tql.com>
2022-05-05 12:14:32 -0400

    Add project files.

---
+++ Banking/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking {
+    internal class Account {
+
+        private static int NextId = 1;  //static because it only needs to start at 1 once, then increase
+        //Properties Below
+        public int AcctNbr { get; private set; } = 0;  //public to read, private to set the values
+        public string Name { get; set; } = string.Empty;  //An Account needs a Name
+        public decimal Balance { get; private set; } = 0;  // An Account needs a Balance. Cant changeunless in Account class
+
+        //Methods Below
+        public bool Deposit(decimal Amount) {      //Creating a Deposit method(action). Can Deposit decimal amount
+            if (Amount <= 0) {
+                Console.WriteLine("Amount must be greater than 0");     //To deposit money, it must be greater than 0
+                return false;                                       //if amount is less than 0, return
+            }
+            Balance = Balance + Amount;  //This shows the balance amount after depositing
+            return true;
+        }
+        public bool Withdraw(decimal Amount) {      //Creating a Withdraw method(action). 
+            if (Amount <= 0) {                  //To Withdraw money, it must be more than 0
+                Console.WriteLine("Amount must be greater than 0");   
+                return false;
+            }
+                if (Amount > Balance) {
+                Console.WriteLine("Insufficent Funds");     // if it is more than the balance amount, 
+                return false;                               // Display Insufficent funds and return false
+            }
+            Balance = Balance - Amount;                     //if all is good-- execute and pull money from
+            return true;
+        }
+
+        public bool Transfer(decimal Amount, Account ToAccount) {  // Creating a Transfer method(action)
+            var Success = (Withdraw(Amount));           //creating a Variable to declare if the amount will draw
+            if (Success) {                              // do the withdraw, and if it is successful, do the next line
+                ToAccount.Deposit(Amount);              //take the withdraw amount, and deposit it to ToAccount account
+                return true;
+            }
+            return false;
+        }
+
+        public Account() {
+            AcctNbr = NextId++;
+        }
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,41 @@
+﻿using Banking;
+
+var sav1 = new Savings();
+sav1.Name = "My Savings";
+
+sav1.Deposit(100);
+sav1.PayInterest(8);
+Console.WriteLine($"The balance is {sav1.Balance:c}") ;
+
+
+
+/*
+var acct1 = new Account() {           // Creating a new instance of a class
+    Name = "Checking 1"               //Naming the 'Account Checking 1'
+};
+var acct2 = new Account() {            //Creating 2nd account
+    Name = "Checking 2"                //Naming it Checking 2
+};
+
+
+
+acct1.Deposit(1000);                   //Depositing 1k into Acct1
+acct2.Deposit(500);                    //Depositing 500 into Acct2
+
+acct1.Withdraw(200);                    //Withdrawing 200 from Acct1
+acct2.Withdraw(50);                     //Withdrawing 50 from Acct2
+
+Console.WriteLine($"Acct1 Balance is {acct1.Balance:c}. Acct2 Balance is {acct2.Balance:c}.");  //to reference the 
+                                                                                                //:c makes it currency
+acct1.Withdraw(1000);
+
+Console.WriteLine($"Acct1 Balance is {acct1.Balance:c}. Acct2 Balance is {acct2.Balance:c}."); //tried to withdraw more money than acct has.
+                                                                                               // came back insufficent funds and canceled
+acct2.Transfer(100, acct1);
+
+Console.WriteLine($"Acct1 Balance is {acct1.Balance:c}. Acct2 Balance is {acct2.Balance:c}."); //transferred 100 from acct2 -> acct1
+
+acct1.Deposit(-100);
+
+Console.WriteLine($"Acct1 Balance is {acct1.Balance:c}. Acct2 Balance is {acct2.Balance:c}."); 
+*/
